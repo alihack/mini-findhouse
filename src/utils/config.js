8abe04851b0880067b9fe7747ad7329821c193fd
陈立 <chenli@chenlideMacBook-Air.local>
2018-12-24 17:31:26 +0800

    下拉菜单

--- src/utils/config.js
+++ src/utils/config.js
@@ -49,6 +49,32 @@ const supportArry = [{
 		markerIcon: '../images/marker-bank.png'
 	}]
 
+// 排序工具栏
+const sortData = [{
+			title: '区域',
+			isSelected: false,
+			kinds: ['不限', '湖里区', '集美区', '海沧区', '翔安区', '同安区', '周边']
+		},
+		{
+			title: '总价',
+			isSelected: false,
+			kinds: ['不限', '100万以下', '100-200万', '200-300万', '300-400万', '400-500万', '500-800万', '800万以上']
+		},
+		{
+			title: '户型',
+			isSelected: false,
+			kinds: ['不限', '一室', '二室', '三室', '四室', '五室', '五室以上']
+		},
+		{
+			title: '更多',
+			isSelected: false,
+			type: [
+				{title: '面积', kinds: ['50平以下', '50-70平', '70-90平', '90-110平', '110-130平', '130-150平', '150-200平', '200平以上']},
+				{title: '朝向', kinds: ['朝东', '朝南', '朝西', '朝北', '南北']},
+			]
+		}
+	]
 module.exports = {
-	supportArry
+	supportArry,
+	sortData
 }
