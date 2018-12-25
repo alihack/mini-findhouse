84c3de3bb37cb3be6d6fb69a915ac2b5daad3484
陈立 <chenli@chenlideMacBook-Air.local>
2018-12-25 17:23:32 +0800

    经纪人

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,12 @@
 const roots = {
-	prod: 'https://api.hd.xiaoyu.com/wxapp/fngj/index/',
-	dev: 'http://192.168.1.46/index.php/wxapp/fngj/index/',
-	test: 'https://hd-api.dev.xiaoyu.com/index.php/wxapp/fngj/index/'
+	prod: 'https://api.hd.xiaoyu.com/wxapp/fnzf/index/',
+	dev: 'http://192.168.1.46/index.php/wxapp/fnzf/index/',
+	test: 'https://hd-api.dev.xiaoyu.com/index.php/wxapp/fnzf/index/'
 }
 const apis = [
-	'getUserId',
-	'saveUser',
-	'getUserInfo',
-	'getEvaluationHistory',
-	'getCommunityList',
-	'getHousePrice',
-	'addHouseInfo',
-	'sendCode',
-	'mobileLogin',
-	'addUserIntention',
-	'getUserMobile',
-	'getTrend',
-	'isSubscribe',
-	'getSearchHistory'
+	'followList',
+	'history',
+	'houseList'
 ]
 
 const env = 'test' // dev: 本地测试服务器， prod：线上服务器, test：测试服务器
